e32e250774d01359241759bb96955cbdfb48b735
abem93 <aimejia4@gmail.com>
2024-01-24 03:55:45 -0600

    Lesson 3 completed

---
+++ Lesson-3.sql
@@ -0,0 +1,21 @@
+--  Exercise 3 — Tasks
+
+--     Find all the Toy Story movies 
+
+      SELECT * FROM movies 
+      WHERE title LIKE "Toy Story%";
+
+--     Find all the movies directed by John Lasseter
+
+      SELECT * FROM movies 
+      WHERE director = "John Lasseter";    
+
+--     Find all the movies (and director) not directed by John Lasseter
+
+      SELECT * FROM movies 
+      WHERE director != "John Lasseter"; 
+
+--     Find all the WALL-* movies
+
+    SELECT * FROM movies 
+    WHERE title LIKE "WALL-_";
