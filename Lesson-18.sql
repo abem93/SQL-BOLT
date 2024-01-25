4da2643853d3684df64bec8aec21244f850f7090
abem93 <aimejia4@gmail.com>
2024-01-24 18:12:48 -0600

    Lesson 18 completed

---
+++ Lesson-18.sql
@@ -0,0 +1,9 @@
+--  Exercise 18 — Tasks
+
+--     We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+
+DROP TABLE Movies;
+
+--     And drop the BoxOffice table as well
+
+DROP TABLE BoxOffice;
