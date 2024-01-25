3de84f50e98884e8b6cd6b190021581a9e6dbb84
abem93 <aimejia4@gmail.com>
2024-01-24 18:11:04 -0600

    Lesson 17 completed

--- Lesson-17.sql
+++ Lesson-17.sql
@@ -9,3 +9,6 @@ SELECT * FROM movies;
 
 --     Add another column named Language with a TEXT data type to store the language that the movie was released in. Ensure that the default for this language is English.
 
+ALTER TABLE Movies
+ADD Language TEXT
+DEFAULT "English";
